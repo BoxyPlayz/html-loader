e33149d252111829eed2bdfa88550814d1afcbc0
Boxy <nozakisola@gmail.com>
2024-12-11 17:58:46 -0600

    Base Functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+updateIncludes();
+
+const updateIncludes = () => {
+  const allInclude = document.querySelectorAll("include");
+  for (let i = 0; i < allInclude.length; i++) {
+    const x = allInclude[i];
+    const location = x.getAttribute("location");
+
+    fetch(location)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then((data) => {
+        x.innerHTML = data;
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+};
+
+export default updateIncludes
